341c8b92bf893e574bcc8bd7fc61569d43eee3ee
EC2 Default User <ec2-user@ip-172-31-6-32.ec2.internal>
2019-11-09 22:39:36 +0000

    Version 2

--- status_handler.go
+++ status_handler.go
@@ -1,44 +1,5 @@
 package main
 
-import (
-	"html/template"
-)
-
-var layoutMarkup = `
-	{{define "layout"}}
-		<!DOCTYPE html>
-		<html>
-		<head>
-			<style>
-				body {
-					max-width: 800px;
-					margin: 2rem auto;
-				}
-
-				header {
-					background-color: #8ac5c3;
-				}
-
-				footer {
-					background-color: #edece8;
-				}
-			</style>
-			<title>Project status report</title>
-		</head>
-		<body>
-			<header></header>
-			{{block "content" .}}
-
-			{{end}}
-			<footer>This is the footer</footer>
-		</body>
-	{{end}}
-`
-
-var indexMarkup = `
-{{define "content"}}
-	<p>
-		
-    </p>
-{{end}}
-`
+func statusHandler(w http.ResponseWriter, r *http.Request) {
+	statusTemplate.Execute(w, fmt.Sprintf("Project status 2"))
+}
