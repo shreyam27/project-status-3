7bcbba14a0acbff9485e32f58be8ebda2de79d56
EC2 Default User <ec2-user@ip-172-31-6-32.ec2.internal>
2019-12-03 09:25:13 +0000

    Final project

--- status.go
+++ status.go
@@ -32,15 +32,23 @@ var statusMarkup = `
 
 <div style="text-align:center">
     <h1>Inner Surf Coding Team</h1>
-    <h3>Project Status Report 3</h3>
-    <span>November 16</span><br>
+    <h3>Final Status Report</h3>
+    <span>Decemeber 5</span><br>
 </div>
 <div>
 <h3>Burndown chart</h3>
-<img src="https://i.imgur.com/CAwpbfI.png" height="775" width="1000">
+<img src="https://i.imgur.com/CAwpbfI.png" height="850" width="1000">
 <img src="https://i.imgur.com/q0ZVbGV.png" height="450" width="1000">
-<h3> Sprint Completed Stories</h3>
+<h3>Completed Stories across all sprints</h3>
 <ul>
+<li>View about page - Enhancement</li>
+<li>Create event - Enhancement</li>
+<li>RSVP related database update - DB Schema Update</li>
+<li>RSVP for the event - Enhancement</li>
+<li>View event details - Enhancement</li>
+<li>Images in events - Bug</li>
+<li>Logo in header and footer - Enhancement</li>
+<li>Upload app on hosting platform</li>
 <li>AB testing (via programming) - Enhancement</li>
 <li>Track analytics - Google analytics</li>
 <li>Error handling for create page - Enhancement</li>
@@ -48,22 +56,24 @@ var statusMarkup = `
 <li>Confirmation code for RSVP - Enhancement</li>
 <li>Time incorrectly parsed in new events - Bug</li>
 <li>Bootstrap CSS inclusion - Enhancement</li>
-</ul>
-<p>We worked on setting up Google analytics and programtically performed AB testing. Additionally, we worked on error handling for create and RSVP page. We also added the hash based confirmation code for the email after RSVP. We also fixed bugs. We faced challenges related to Google Optimize for AB testing and therefore decided to use a programming approach.</p>
-</div>
-<div>
-<h3>Product Backlog</h3>
-<p>We will focus on APIs, bug fixing and aesthetic aspects of the app in the remaining time period. We will check the user preference as part of AB testing via tracking the contents of the Heroku DB where we are storing this data.
-<ul>
 <li>APIs for the app</li>
 <li>Bug fixing</li>
 <li>Aesthetics of the app</li>
+<li>Prject status report</li>
 </ul>
+<p>We improved with each spirnt restrospective. In the first Sprint we familarized ourseleves with the starter code and completed simple functionalities. In the second sprint we completed the key stories. In the third sprint we focused on AB testing and additional enhancements  </p>
 </div>
-<div>
-<h3>Google Analytics</h3>
-<p> Google analytics data on the traffic flow from different groups of users follows: </p>
-<img src="https://i.imgur.com/zf6eG2k.png" height="400" width="1000">
+<section>
+<h3>Traffic analysis - Google analytics</h3>
+<p>The percentage of traffic from each of the referres follows: </p>
+<img src="https://i.imgur.com/CAwpbfI.png" height="850" width="1000">
+</section>
+<section>
+<h3>AB testing</h3>
+<p>"Donate" is more effective than "Support". The percentage of users clicking on "Donate" of the total is 75% of the total clicks on the link whereas 'Support' is 25% of the percentage of the total clicks. We tracked the database counts programmatically. The count of clicks tracked in the database follows:</p>
+<img src="https://i.imgur.com/oxbltrV.png" height="275" width="450">
+</section>
+
 {{end}}
 `
 
